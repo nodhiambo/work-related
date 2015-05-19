565997586f58b025cc5ef3edd18ab6453611c404
Nicholas Odhiambo <tripledijitt@gmail.com>
2015-05-19 04:15:38 -0400

    Description entry changes

--- codeChallenge.py
+++ codeChallenge.py
@@ -18,95 +18,115 @@ def cleanhtml(raw_html):
 # For each entry (such as individual blog posts or news stories) in the feed display:
 # 1. Title
 # 2. URL
+####################################################################################### 
+def processURL(myUrl):
+  strBuild = ""
+  try:
+    # Parse url
+    feeds = feedparser.parse(myUrl)
+    print("Please wait for processing to be completed. It may take several minutes...")
+    print ("URL of the feed: "+myUrl)
+    print ("Total entries: "+str(len(feeds['entries'])))
+    #print ("URL of the feed: "+feeds['feed']['link'])
+    errorMsg = "There is no entry description or summary"
+    for post in feeds.entries:
+      print("\n")
+      print("Feed Title: "+post.title)
+      print("URL of the individual entry: "+post.link+"\n")
+      try:
+        if post.description != None or post.summary != None:
+          if post.description:
+            #obtain only individual entries
+            try:
+              soup = BeautifulSoup(post.description)
+              soup = soup.findAll(text=True)
+              # Remove all the endlines from the sentences
+              cleansed = [a for a in soup if a != '\n']
+              # Remove preceding u' from all the sentences aka encoding
+              encoded =  [x.encode('utf-8') for x in cleansed]
+              for text in encoded:
+                if text:
+                  strBuild += text 
+              #Remove ending spaces      
+              if strBuild.endswith('  '):
+                strBuild = strBuild[:-4]      
+            except Exception, e:
+              print (str(e))  
+          else:
+            # If there is no entry description and there is a summary 
+            try:
+              soup = BeautifulSoup(post.summary)
+              soup = soup.findAll(text=True)
+              # Remove all the endlines from the sentences
+              cleansed = [a for a in soup if a != '\n']
+              # Remove preceding u' from all the sentences aka encoding
+              encoded =  [x.encode('utf-8') for x in cleansed]
+              for text in encoded:
+                if text:
+                  strBuild += text 
+              #Remove ending spaces      
+              if strBuild.endswith('  '):
+                strBuild = strBuild[:-4]   
+            except Exception, e:
+              print (str(e))    
+        else:
+           print(errorMsg)
+      except Exception, e:
+        print (str(e))  
+      strBuild = cleanhtml(strBuild) 
+      processEntrySentiment(strBuild, errorMsg)  
+      # Initialize string and array
+      strBuild = ""    
+  except Exception, e:
+    print (str(e))  
+  return
+
+######################################################################################
+# Process for each entry:
 # 3. Sentiment expressed in the entry's text This should have the following format:
 #    - positive indicators: [list of positive indicators]
 #    - negative indicators: [list of negative indicators]
 #    - overall sentiment: <"positive"|"negative"|"neutral"> 
-####################################################################################### 
-def processURL(myUrl):
-    negCount = 0
-    posCount = 0
-    posList = []
-    negList = []
-    sentenceArray = []
-    strBuild = ""
-    try:
-      # Parse url
-      feeds = feedparser.parse(myUrl)
-      print ("Total entries: "+str(len(feeds['entries'])))
-      #print ("URL of the feed: "+feeds['feed']['link'])
-      print ("URL of the feed: "+myUrl)
-      for post in feeds.entries:
-        print("Feed Title: "+post.title+"\n")
-        print("Feed URL: "+post.link+"\n")
-        try:
-          if post.description or post.summary:
-            if post.description:
-              #obtain only individual entries
-              try:
-                soup = BeautifulSoup(post.description)
-                soup = soup.findAll(text=True)
-                # Remove all the endlines from the sentences
-                cleansed = [a for a in soup if a != '\n']
-                # Remove preceding u' from all the sentences aka encoding
-                encodedArray =  [x.encode('utf-8') for x in cleansed]
-                for text in cleansed:
-                  strBuild += cleanhtml(text) 
-              except Exception, e:
-                print (str(e))      
-            else:
-              # If there is no entry description and there is a summary 
-              try:
-                soup = BeautifulSoup(post.summary)
-                soup = soup.findAll(text=True)
-                # Remove all the endlines from the sentences
-                cleansed = [a for a in soup if a != '\n']
-                # Remove preceding u' from all the sentences aka encoding
-                encodedArray =  [x.encode('utf-8') for x in cleansed]
-                for text in cleansed:
-                  strBuild += cleanhtml(text) 
-              except Exception, e:
-                print (str(e))        
-          else:
-             print("There are no entry descriptions or summary")
-        except Exception, e:
-          print (str(e))  
-         
-      print("Please wait for the processing to be completed. It may take several minutes...")  
-      text = TextBlob(strBuild)
-      count = 0                 
-      for sentence in text.sentences:
-        if count < 5:    #Process only five sentences to shorten the processing time
-          #sentenceArray.append(str(sentence))
-          blob = TextBlob(str(sentence), analyzer=NaiveBayesAnalyzer())
-          if blob.sentiment.classification == 'neg':
-            negCount += 1
-            negList.append(str(sentence))
-          else:
-            posCount += 1
-            posList.append(str(sentence))
-          count += 1   
-          print("Processing sentence: "+str(count))
-      
-      # Another module sentiment indicator to confirm results by comparison 
-      # pos_score, neg_score = senti_classifier.polarity_scores(sentenceArray)
-      # print (pos_score, neg_score)
-      
-      print ("Negative Indicators: ")    
-      print (negList)
-      print ("Positive Indicators: ")
-      print (posList)
-      if negCount == posCount:                                                                    
-        print ("Overall Sentiment: Neutral")
-      else:
-        if negCount > posCount:
-          print ("Overall Sentiment: Negative")
+#######################################################################################    
+def processEntrySentiment(strText, msg):
+  negCount = 0
+  posCount = 0
+  posList = []
+  negList = []
+  sentenceArray = [] 
+  if strText:  
+    text = TextBlob(strText)
+    count = 0                 
+    for sentence in text.sentences:
+        sentenceArray.append(str(sentence))
+        blob = TextBlob(str(sentence), analyzer=NaiveBayesAnalyzer())
+        if blob.sentiment.classification == 'neg':
+          negCount += 1
+          negList.append(str(sentence))
         else:
-          print ("Overall Sentiment: Positive")  
-    except Exception, e:
-      print (str(e))
+          posCount += 1
+          posList.append(str(sentence))   
+    
+    # Another module sentiment indicator to confirm results by comparison 
+    pos_score, neg_score = senti_classifier.polarity_scores(sentenceArray)
+    print ("Comparative Positive score: "+str(pos_score))
+    print ("Comparative Negative score: "+str(neg_score))
+     
+    print ("Negative Indicators: ")    
+    print (negList)
+    print ("Positive Indicators: ")
+    print (posList)
+    if negCount == posCount:                                                                    
+      print ("Overall Sentiment: Neutral")
+    else:
+      if negCount > posCount:
+        print ("Overall Sentiment: Negative")
+      else:
+        print ("Overall Sentiment: Positive") 
+  else:
+    print(msg)
+  return
       
-    return  
       
 # Get the total number of args passed
 total = len(sys.argv)
